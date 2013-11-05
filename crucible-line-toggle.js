9ffd0dc3c6eb2e56988977525fbdc6b3571f1d75
Robert Smith <robert.smith@taitradio.com>
2013-11-06 11:30:35 +1300

    Should work now

--- crucible-line-toggle.js
+++ crucible-line-toggle.js
@@ -9,7 +9,7 @@
 // @updateURL      https://raw.github.com/emeraldo/crucible-line-toggle/master/crucible-line-toggle.js
 // ==/UserScript==
 
-var isLineNumberingOff = true;
+var isLineNumberingOff = false;
 var nodes = null;
 var originalTexts = new Array();
 
@@ -19,29 +19,30 @@ window.addEventListener("DOMContentLoaded", function() {
     {
         originalTexts[i] = nodes[i].innerHTML;
     }
-    switch_button = document.createElement("button")
-    switch_button_text = document.createTextNode("Toggle line numbers")
-    switch_button.appendChild(switch_button_text)
-    
-    switch_button.onclick = function() { 
-        if (isLineNumberingOff) {
-            for (var i = 0; i < nodes.length; i++)
-            {
-                nodes[i].innerHTML = originalTexts[i];
-            }
-            isLineNumberingOff = false;
-        }
-        else {
-            for (var i = 0; i < nodes.length; i++)
-            {
-                nodes[i].innerHTML = '';
-            }
-            isLineNumberingOff = true;
-        }
-    };
-    var toolbar = document.getElementsByClassName("toolbar")[0]
-    toolbar.appendChild(switch_button);
 }, false);
+
+switch_button = document.createElement("button")
+switch_button_text = document.createTextNode("Toggle line numbers")
+switch_button.appendChild(switch_button_text)
+
+switch_button.onclick = function() { 
+    if (isLineNumberingOff) {
+        for (var i = 0; i < nodes.length; i++)
+        {
+            nodes[i].innerHTML = originalTexts[i];
+        }
+        isLineNumberingOff = false;
+    }
+    else {
+        for (var i = 0; i < nodes.length; i++)
+        {
+            nodes[i].innerHTML = '';
+        }
+        isLineNumberingOff = true;
+    }
+};
+var toolbar = document.getElementsByClassName("toolbar")[0]
+toolbar.appendChild(switch_button);
 
 
             

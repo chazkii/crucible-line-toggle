ef098e9342b8532b8d3fc42841de9a97f3b94703
Robert Smith <robert.smith@taitradio.com>
2013-11-07 11:18:54 +1300

    Made it work

--- crucible-line-toggle.js
+++ crucible-line-toggle.js
@@ -4,22 +4,30 @@
 // @description    Toggle line numbering in Crucible
 // @version        1.0
 // @include        http://crucible.*
-// @author         Charlie Smith
+// @author         Charlie Smith and Mark Cottrell
 // @email          r.charles.smith@gmail.com
 // @updateURL      https://raw.github.com/emeraldo/crucible-line-toggle/master/crucible-line-toggle.js
 // ==/UserScript==
 
 var isLineNumberingOff = false;
 var nodes = null;
-var originalTexts = new Array();
 
-window.addEventListener("DOMContentLoaded", function() {
+function storeNodes () {
     nodes = document.body.querySelectorAll(".tetrisColumn, .diffNav, .author, .revision, .diffLineNumbers, .diffLineNumbersA, .diffLineNumbersB");
-    for (var i = 0; i < nodes.length; i++)
-    {
-        originalTexts[i] = nodes[i].innerHTML;
-    }
-}, false);
+}
+
+window.addEventListener("DOMContentLoaded", storeNodes, false);
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(storeNodes);
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document.getElementById("frxs"), {
+  subtree: true,
+  attributes: true
+});
 
 switch_button = document.createElement("button")
 switch_button_text = document.createTextNode("Toggle line numbers")
@@ -29,20 +37,19 @@ switch_button.onclick = function() {
     if (isLineNumberingOff) {
         for (var i = 0; i < nodes.length; i++)
         {
-            nodes[i].innerHTML = originalTexts[i];
+            nodes[i].style.display = "none";
         }
         isLineNumberingOff = false;
     }
     else {
         for (var i = 0; i < nodes.length; i++)
         {
-            nodes[i].innerHTML = '';
+            nodes[i].style.display = "";
         }
         isLineNumberingOff = true;
     }
 };
 var toolbar = document.getElementsByClassName("toolbar")[0]
 toolbar.appendChild(switch_button);
-
 
             
